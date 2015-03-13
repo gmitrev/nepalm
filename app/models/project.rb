e4f71c65ffdb0d04b71987bc37b6603eee389c0a
gmitrev <gvmitrev@gmail.com>
2015-03-13 21:32:14 +0200

    Improved layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,4 +13,26 @@ class Project < ActiveRecord::Base
   has_many :stacks
 
   belongs_to :owner, class_name: "User"
+
+  def users
+    stacks.flat_map(&:users)
+  end
+
+  def completed_tasks
+    completed = stacks.flat_map(&:completed_tasks_count).reduce(:+) || 0
+    total = stacks.flat_map(&:total_tasks_count).reduce(:+) || 0
+
+    "#{completed}/#{total}"
+  end
+
+  def completed_percentage
+    completed = stacks.flat_map(&:completed_tasks_count).reduce(:+) || 0
+    total = stacks.flat_map(&:total_tasks_count).reduce(:+) || 0
+
+    if total.zero? || completed.zero?
+      0
+    else
+      (completed.to_f / total.to_f) * 100
+    end
+  end
 end
--- app/models/stack.rb
+++ app/models/stack.rb
@@ -25,23 +25,23 @@ class Stack < ActiveRecord::Base
     end
   end
 
-  def completed_tasks
-    tasks = task_list.tasks
-    total = tasks.count
-    completed = tasks.select(&:completed?).count
+  def completed_tasks_count
+    task_list.tasks.select(&:completed?).count
+  end
 
-    "#{completed}/#{total}"
+  def total_tasks_count
+    task_list.tasks.count
+  end
+
+  def completed_tasks
+    "#{completed_tasks_count}/#{total_tasks_count}"
   end
 
   def completed_percentage
-    tasks = task_list.tasks
-    total = tasks.count
-    completed = tasks.select(&:completed?).count
-
-    if total.zero? || completed.zero?
+    if total_tasks_count.zero? || completed_tasks_count.zero?
       0
     else
-      (completed.to_f / total.to_f) * 100
+      (completed_tasks_count.to_f / total_tasks_count.to_f) * 100
     end
   end
 
