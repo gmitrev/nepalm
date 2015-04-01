d147241795eb08d9a8a138ee0dd49d060b1e82a3
gmitrev <gvmitrev@gmail.com>
2015-04-01 21:01:59 +0300

    Nuke organizations

--- app/views/organizations/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @organization, :id, :name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  # resources :comments
-
   resources :memberships
-
-  # resources :organizations do
-  #   member do
-  #     get :new_members
-  #     post :add_members
-  #   end
-  # end
 
   resources :projects do
     collection do
