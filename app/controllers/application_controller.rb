3bfe82b3662435bb3ee1ad4bd24c5ebf5e5d7ac8
gmitrev <gvmitrev@gmail.com>
2015-08-19 20:11:32 +0300

    Redirect to dashboard after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :admin_for?
 
+  def after_sign_in_path_for(resource)
+    projects_path
+  end
+
   protected
 
   def configure_permitted_parameters
